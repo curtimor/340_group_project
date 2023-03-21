24fb36baf778df6490703674a2cedbb739fc46c0
EricDYork <102713620+EricDYork@users.noreply.github.com>
2023-03-20 22:36:47 -0700

    Update add_sale.js

--- final_draft_code/public/js/add_sale.js
+++ final_draft_code/public/js/add_sale.js
@@ -1,3 +1,8 @@
+//Citation for the following page:
+//Date: 3-20-2022
+//This page is adapted from the Node JS starter code app.js file
+//Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
+
 // Get the objects we need to modify
 let addSaleForm = document.getElementById('add-sale-form-ajax');
 
