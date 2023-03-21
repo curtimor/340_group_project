5cdd94aadcc2dafc3d91eb732706efff410bea68
EricDYork <102713620+EricDYork@users.noreply.github.com>
2023-03-20 22:37:40 -0700

    Update delete_order.js

--- final_draft_code/public/js/delete_order.js
+++ final_draft_code/public/js/delete_order.js
@@ -1,3 +1,8 @@
+//Citation for the following page:
+//Date: 3-20-2022
+//This page is adapted from the Node JS starter code app.js file
+//Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
+
 //code for deleteOrder function using jQuery
 function deleteOrder(idOrder) {
     let link = '/delete-order-ajax/';
